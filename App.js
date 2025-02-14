a500e85e862e3feda8dae08c0b2bac84ab5ae24c
davidoderso <stranz1david@gmail.com>
2025-02-14 13:31:27 +0100

    OSM Integration in App + Anzeige der Dönerläden in der angegebenen Straße

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import StreetMap from "./app/(tabs)/openStreetMapScreen";
 
 export default function App() {
     return (
-        <SafeAreaView style={{ flex: 1}}>
-            <StreetMap />
-        </SafeAreaView>
+        <GestureHandlerRootView style= {{felx: 1}}>
+             <SafeAreaView style={{ flex: 1}}>
+                <StreetMap />
+            </SafeAreaView>
+        </GestureHandlerRootView>
     );
 }
--- scripts/styles/mapStyles.js
+++ scripts/styles/mapStyles.js
@@ -1,27 +1,91 @@
 import { StyleSheet } from 'react-native';
 
-const mapStyles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  inputContainer: {
+    position: 'absolute',
+    top: 50, 
+    left: 20,
+    right: 20,
+    zIndex: 1, 
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+    shadowColor: 'black',
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+    borderRadius: 5,
+  },
+  bottomBar: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#ffffff',
+    padding: 10,
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderTopColor: '#ddd',
+  },
+  bottomText: {
+    fontSize: 16,
+    color: 'black',
+  },
+  modalBackground: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Halbtransparenter Hintergrund
   },
-  searchContainer: {
-    position: 'absolute',
-    top: 50,  
-    zIndex: 10,  
+  modalContainer: {
+    backgroundColor: 'white',
+    padding: 20,
     width: '80%',
-  },
-  loadingText: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    maxHeight: '50%', // Die Höhe des Modals auf 50% des Bildschirms begrenzen
+    borderRadius: 10,
   },
   listItem: {
     padding: 10,
-    marginVertical: 5,
-    backgroundColor: '#f0f0f0',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  listItemText: {
+    fontSize: 16,
+  },
+  searchIcon: {
+    fontSize: 30,
+    color: '#000',
+  },
+  searchIconContainer: {
+    position: 'absolute',
+    top: 50,
+    right: 20,
+    zIndex: 1,
+  },
+  donerShopsFoundContainer: {
+    padding: 10, 
+    alignItems: 'center',
+    zIndex: 1,
+  },
+  donerShopsFoundText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000',
+    zIndex: 1,
+  },
+  mapstyle: {
+    flex: 1,
   },
 });
 
-export default mapStyles;
+export default styles;
+
